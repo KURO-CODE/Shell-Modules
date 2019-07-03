4787b71878bb9bb6f664983ffc7ef9597b8f5634
KURO <kurocode404@gmail.com>
2019-07-03 19:55:56 +0200

    Update ChkDist.sh

--- Check/Distro/ChkDist.sh
+++ Check/Distro/ChkDist.sh
@@ -7,7 +7,40 @@
 # Coded By Kuro-Code
 
 #~~~~ Check Distro ~~~~
-
-Vname=`lsb_release -a`
+Vname0=`lsb_release -a`
+Vname=`lsb_release -a | grep ID | awk '{print $3}'` # >>> Var Distro Name <<<
 clear
-echo $Vname | grep ID | awk '{print $3,$6,$7}'
+case $Vname in
+	"Ubuntu")
+		echo -e $Vname0 | grep ID | awk '{print $3,$6,$7}' 
+		sleep 3
+#         *** example *** 
+#		device="wlp2s0" >>> Var Wireless card <<<
+#		Mon="mon0" >>> Monitor Mode <<<
+#		$airmon start "$device"
+	;;
+	"Kali")
+		echo $Vname0 | grep ID | awk '{print $3,$6,$7}'
+		sleep 3
+#         *** example ***
+#		device="wlan0" >>> Var Wireless card <<<
+#		Mon2="mon" >>> Var <<<
+#		Mon="$device$Mon2" >>> Var Monitor Mode <<<
+#		$airmon start "$device"
+	;;
+
+	"LinuxMint")
+		echo $Vname0 | grep ID | awk '{print $3,$6,$7}'
+		sleep 3
+#         *** example ***
+#		...
+	;;
+
+	"")
+		Vname0=`cat /etc/redhat-release |awk '{print $1}'`
+		echo -e "$Vname0"
+		sleep 3
+#         *** example ***
+#		...
+	;;
+	esac 
