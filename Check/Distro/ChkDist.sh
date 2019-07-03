282cc86863dfbb9a319ef4009180bfbca31ba329
KURO <kurocode404@gmail.com>
2019-07-03 17:14:23 +0200

    Create ChkDist.sh

---
+++ Check/Distro/ChkDist.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Check Distro Module
+#
+# Dev: Shell
+# Date: 03/07/2019
+# Coded By Kuro-Code
+
+#~~~~ Check Distro ~~~~
+
+Vname=`lsb_release -a`
+clear
+echo $Vname | grep ID | awk '{print $3,$6,$7,$11}'
